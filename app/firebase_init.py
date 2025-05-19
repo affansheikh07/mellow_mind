d6a8186cd7b8946883bb0e5eea1594e4be7f6e5c
Affan <affansheikh156@gmail.com>
2025-05-19 17:50:33 +0500

    Social sign in test

--- app/firebase_init.py
+++ app/firebase_init.py
@@ -1,6 +1,17 @@
-# import firebase_admin
-# from firebase_admin import credentials
+import firebase_admin
+from firebase_admin import credentials
+import os
 
-# if not firebase_admin._apps:
-#     cred = credentials.Certificate("mellow-mind-b65e9-firebase-adminsdk-fbsvc-232f36fe5a.json")
-#     firebase_admin.initialize_app(cred)
+cred = credentials.Certificate({
+    "type": os.environ["FIREBASE_TYPE"],
+    "project_id": os.environ["FIREBASE_PROJECT_ID"],
+    "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
+    "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace('\\n', '\n'),
+    "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID", ""),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL", "")
+})
+firebase_admin.initialize_app(cred)
